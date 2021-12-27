e60cc2639545b064cf7dc69a515cac4b15cd235a
Krunal Patel <krunalkachadia@gmail.com>
2021-12-27 13:00:23 -0500

    Update threejs site 1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import '/style.css';
 // import * as THREE from 'three';
 import * as THREE from './node_modules/three/build/three.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
 
 // Setup
 
